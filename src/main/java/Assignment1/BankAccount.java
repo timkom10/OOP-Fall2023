af2b5d43ee785e3463a1822ac11bbb6cf19b731a
Tim <47118765+timkom10@users.noreply.github.com>
2023-09-23 16:16:43 -0500

    instance Variables for q1

--- src/main/java/Assignment1/BankAccount.java
+++ src/main/java/Assignment1/BankAccount.java
@@ -1,4 +1,9 @@
 package Assignment1;
 
 public class BankAccount {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+    private enum accountType {SAVINGS, CHECKING};
+
 }
