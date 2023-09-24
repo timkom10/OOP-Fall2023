e9baf256e43f4c250848313b6b9f5664b7e0e29f
Tim <47118765+timkom10@users.noreply.github.com>
2023-09-23 20:17:22 -0500

    Q2 and added get account info for q1

---
+++ src/main/java/Assignment1/Question1/BankAccount.java
@@ -0,0 +1,42 @@
+package Assignment1.Question1;
+public class BankAccount {
+    private final int accountNumber;
+    private final String accountHolderName;
+    accountType type;
+    private double balance;
+
+    public BankAccount(int accountNumber, String accountHolderName, double initialBalance, accountType type) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+        this.type = type;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            this.balance = amount + this.balance;
+            System.out.printf("Current Balance is: %f %n", this.balance);
+        } else {
+            System.out.println("Invalid Amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= this.balance) {
+            this.balance = this.balance - amount;
+            System.out.printf("Current Balance is: %f %n", this.balance);
+        } else {
+            System.out.println("Invalid Amount");
+        }
+    }
+
+    public void getAccountInfo() {
+        System.out.printf("Account Number: %d, Account Holder: %s, Account Type: %s, Balance: $%.2f", this.accountNumber, this.accountHolderName, this.type, this.balance);
+    }
+
+    private enum accountType {SAVINGS("SAVINGS"), CHECKING("CHECKING");
+    private String span;
+    accountType(String string){
+        span = string;
+    }}
+}
