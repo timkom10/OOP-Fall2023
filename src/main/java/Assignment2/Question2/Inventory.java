66f79f1107cab54119505568c0ce694147c84e27
Tim <47118765+timkom10@users.noreply.github.com>
2023-10-07 02:42:07 -0500

    Inventory additions

--- src/main/java/Assignment2/Question2/Inventory.java
+++ src/main/java/Assignment2/Question2/Inventory.java
@@ -1,4 +1,44 @@
 package Assignment2.Question2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inventory {
+    int inventoryID;
+    VehicleType type;
+    List<Vehicle> vehicles = new ArrayList<>();
+
+    public Inventory(int inventoryID, VehicleType type){
+        this.inventoryID = inventoryID;
+        this.type = type;
+    }
+
+    public VehicleType getType() {
+        return type;
+    }
+
+    public void setType(VehicleType type) {
+        this.type = type;
+    }
+
+    public int getInventoryID() {
+        return inventoryID;
+    }
+
+    public void setInventoryID(int inventoryID) {
+        this.inventoryID = inventoryID;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        if (vehicle.getType() == this.type) {
+            this.vehicles.add(vehicle);
+        } else {
+            System.out.println("Incorrect vehicle type");
+        }
+    }
+
+    public void removeVehicle(Vehicle vehicle){
+            Boolean isRemoved = this.vehicles.remove(vehicle);
+            System.out.println(isRemoved);
+        }
 }
