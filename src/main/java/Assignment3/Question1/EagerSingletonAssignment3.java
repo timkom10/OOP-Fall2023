32fc55bca051901b77d6ef5f6459050f548ffab1
Tim <47118765+timkom10@users.noreply.github.com>
2023-10-26 22:37:05 -0500

    Question 1 finished

--- src/main/java/Assignment3/Question1/EagerSingletonAssignment3.java
+++ src/main/java/Assignment3/Question1/EagerSingletonAssignment3.java
@@ -1,12 +1,13 @@
 package Assignment3.Question1;
 
 public class EagerSingletonAssignment3 {
-    private static final EagerSingletonAssignment3 instance1 = new EagerSingletonAssignment3();
-    private static final EagerSingletonAssignment3 instance2 = new EagerSingletonAssignment3();
-    private static final EagerSingletonAssignment3 instance3 = new EagerSingletonAssignment3();
+    private static final EagerSingletonAssignment3[] instance = {new EagerSingletonAssignment3(), new EagerSingletonAssignment3(), new EagerSingletonAssignment3()};
     private EagerSingletonAssignment3(){
         new Assignment3();
     }
 
+    public static EagerSingletonAssignment3 getInstance(int index){
+        return instance[index-1];
+    }
 
 }
--- src/main/java/Assignment3/Question1/LazySingletonAssignment3.java
+++ src/main/java/Assignment3/Question1/LazySingletonAssignment3.java
@@ -1,4 +1,16 @@
 package Assignment3.Question1;
 
 public class LazySingletonAssignment3 {
+    private static final LazySingletonAssignment3[]  instance =  new LazySingletonAssignment3[3];
+    private LazySingletonAssignment3(){
+        new Assignment3();
+    }
+
+    public static LazySingletonAssignment3 getInstance(int index){
+        if (instance[index-1] == null){
+            instance[index-1] = new LazySingletonAssignment3();
+        }
+
+        return instance[index-1];
+    }
 }
