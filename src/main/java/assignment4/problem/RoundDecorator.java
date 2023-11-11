3816c908c54f918a6c4fa4ac9f8666446b0f940a
Tim <47118765+timkom10@users.noreply.github.com>
2023-11-11 14:05:17 -0600

    Making it a bit more generic

--- src/main/java/assignment4/problem/RoundDecorator.java
+++ src/main/java/assignment4/problem/RoundDecorator.java
@@ -4,16 +4,16 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class RoundDecorator extends HandlerDecorator {
-    ExpDecorator expDecorator;
+    HandlerDecorator nextDecorator;
     RoundDecorator(Handler handler) {
         super(handler);
-        this.expDecorator = new ExpDecorator(handler);
+        this.nextDecorator = new ExpDecorator(handler);
 
     }
     @Override
     public String goNext(String km, String unit) throws Exception {
         BigDecimal answer = new BigDecimal(this.handler.goNext(km, unit));
         answer = answer.setScale(2, RoundingMode.HALF_UP);
-        return expDecorator.goNext(String.valueOf(answer), unit);
+        return nextDecorator.goNext(String.valueOf(answer), unit);
     }
 }
